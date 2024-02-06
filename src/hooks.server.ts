0c21d3c859b859d3f40889dc8ce2528897106aa9
mkitzmann <mkitzmann@gmail.com>
2024-02-06 22:28:36 +0100

    replace node-schedule with cron

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,30 +1,36 @@
-import schedule from "node-schedule"
-import {getProjectsFromSources} from "./lib/server/crawler";
-import {db} from "./lib/server/db";
-import {projects} from "./lib/server/db/schema";
-import {Project} from "./lib/types";
+import { getProjectsFromSources } from './lib/server/crawler';
+import { db } from './lib/server/db';
+import { projects } from './lib/server/db/schema';
+import { Project } from './lib/types';
+import { CronJob } from 'cron';
 
-const crawlerPrefix = '\x1B[34m[crawler] '
+const crawlerPrefix = '\x1B[34m[crawler] ';
 
 export const addProjectAction = async (projectsInsert: Project[]) => {
 	const start = Date.now();
-	await db.insert(projects).values(projectsInsert).onConflictDoNothing()
+	await db.insert(projects).values(projectsInsert).onConflictDoNothing();
 	const end = Date.now();
-	console.log(`${crawlerPrefix} added ${projectsInsert.length} projects to db in ${end - start}ms`)
+	console.log(`${crawlerPrefix} added ${projectsInsert.length} projects to db in ${end - start}ms`);
 };
 
 export const getProjects = async () => {
 	const start = Date.now();
-	const projectsInsert = await getProjectsFromSources()
+	const projectsInsert = await getProjectsFromSources();
 	const end = Date.now();
-	console.log(`${crawlerPrefix} added ${projectsInsert.length} projects to db in ${end - start}ms`)
-	return projectsInsert
+	console.log(`${crawlerPrefix} found ${projectsInsert.length} projects in ${end - start}ms`);
+	return projectsInsert;
 };
 
-const crawlerJob = schedule.scheduleJob('*/1 * * * *', async () => {
-	console.log(`${crawlerPrefix} started`)
-	const projectsInsert = await getProjects()
-	await addProjectAction(projectsInsert)
-})
+const crawlTask = async () => {
+	console.log(`${crawlerPrefix} started at ${new Date().toISOString()}`);
+	const projectsInsert = await getProjects();
+	await addProjectAction(projectsInsert);
+};
 
+const crawlerJob = CronJob.from({
+	cronTime: '*/1 * * * *',
+	onTick: crawlTask,
+	start: true
+});
 
+await crawlTask();
