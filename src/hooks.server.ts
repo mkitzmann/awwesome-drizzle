ab9d6f87d08c94f386afedb214431223d915d71f
mkitzmann <mkitzmann@gmail.com>
2024-02-06 22:03:33 +0100

    Âadd sc schedule

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,32 @@
 import schedule from "node-schedule"
-import {addProjectAction} from "./lib/server/db/actions";
 import {getProjectsFromSources} from "./lib/server/crawler";
 import {db} from "./lib/server/db";
 import {projects} from "./lib/server/db/schema";
+import {Project} from "./lib/types";
 
-// const crawlerJob = schedule.scheduleJob('*/1 * * * *', async function () {
-	console.log('\x1B[34m[crawler] started')
+const crawlerPrefix = '\x1B[34m[crawler] '
+
+export const addProjectAction = async (projectsInsert: Project[]) => {
+	const start = Date.now();
+	await db.insert(projects).values(projectsInsert).onConflictDoNothing()
+	const end = Date.now();
+	console.log(`${crawlerPrefix} added ${projectsInsert.length} projects to db in ${end - start}ms`)
+};
+
+export const getProjects = async () => {
 	const start = Date.now();
 	const projectsInsert = await getProjectsFromSources()
-	// const projects = []
-await db.insert(projects).values(projectsInsert).onConflictDoNothing()
-	// await addProjectAction(projectsInsert)
 	const end = Date.now();
+	console.log(`${crawlerPrefix} added ${projectsInsert.length} projects to db in ${end - start}ms`)
+	return projectsInsert
+};
+
+const crawlerJob = schedule.scheduleJob('*/1 * * * *', async function () {
+	console.log(`${crawlerPrefix} started`)
+	const projectsInsert = await getProjects()
+	await addProjectAction(projectsInsert)
 	console.log(
-		`\x1B[34m[crawler] found ${projectsInsert.length} projects in ${end - start}ms`
 	);
-// })
+})
+
+
